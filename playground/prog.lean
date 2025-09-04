c3640da6f910b1cf678016f088cd0ef5cc36859f
Gnlow <gnlow@pm.me>
2025-09-04 12:55:30 +0900

    fuu

--- playground/prog.lean
+++ playground/prog.lean
@@ -16,7 +16,7 @@ def evalProg (prog: Program) :=
 
 open Expr List
 
-theorem multi_move_sound (a b: Int) (pre: Program)
+theorem multi_move_sound (a b: Int) (pre post: Program)
   : evalProg (pre ++ [move a, move b] ++ post)
   = evalProg (pre ++ [move (a+b)] ++ post)
   := by simp [
@@ -25,9 +25,31 @@ theorem multi_move_sound (a b: Int) (pre: Program)
     Int.add_assoc,
   ]
 
+def optimize: Program → Program
+  | [] => []
+  | [x] => [x]
+  | (move a) :: (move b) :: rest =>
+    move (a + b) :: optimize rest
+
+theorem optimize_sound (p: Program)
+  : evalProg (optimize p) = evalProg p
+  := by
+    induction p with
+    | nil => rfl
+    | cons x xs ih =>
+      match x with
+      | move a =>
+        match xs with
+        | [] => rfl
+        | cons (move b) rest =>
+          rw [optimize]
+          -- 여기서 막힘
+          sorry
+
 def MyProg := [
   move 10,
   move 20,
 ]
 
 #eval evalProg MyProg
+#eval optimize MyProg
